be05076d401fd65e015b7cab6b0bb6c0a96f5cba
ZabitTank <95601488+ZabitTank@users.noreply.github.com>
2022-01-02 17:01:03 +0700

    fix file path

--- app.py
+++ app.py
@@ -11,8 +11,6 @@ import os
 import urllib3
 
 
-ROOT_PATH = "./model/"
-
 URL_PRETRAIN_MODEL = {
 "yolov4-tiny.weights" : "https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-tiny.weights",
 "yolov4-tiny.cfg ": "https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4-tiny.cfg",
@@ -35,7 +33,7 @@ def download_file(file_name,url):
     weights_warning, progress_bar = None, None
     MEGABYTES = 2.0 ** 20.0
     # check if existed
-    path = ROOT_PATH + file_name
+    path = file_name
     
     response = http.request('GET',url,preload_content=False)
     length = int(response.headers.get("Content-Length"))
@@ -66,7 +64,7 @@ def check_missing_model(url_model,download_file):
     flag_download = False
     
     for file_name in url_model:
-        path = ROOT_PATH + file_name
+        path = file_name
         if not os.path.exists(path):
             flag_download = True        
             st.warning("Missing %s" % file_name)
@@ -78,7 +76,7 @@ def check_update(url_model,update_file):
     flag_update = False
 
     for file_name in url_model:
-        path = ROOT_PATH + file_name
+        path = file_name
        
         os.path.getsize(path)
             
@@ -176,8 +174,8 @@ def main_page_render(modelName,model,labels):
 def load_model(model_name):
     with st.spinner('Loading model...'):
         try:
-            cfg_path = os.path.abspath(ROOT_PATH+model_name+".cfg")
-            weights_path = os.path.abspath(ROOT_PATH+model_name+".weights")
+            cfg_path = os.path.abspath(model_name+".cfg")
+            weights_path = os.path.abspath(model_name+".weights")
     
             net = cv2.dnn.readNetFromDarknet(cfg_path, weights_path)
             
@@ -187,7 +185,7 @@ def load_model(model_name):
             model.setInputScale(1.0 / 255)
             model.setInputSwapRB(True)
             
-            with open(os.path.abspath(ROOT_PATH+model_name+".names"), 'rt') as f:
+            with open(os.path.abspath(model_name+".names"), 'rt') as f:
                 labels = f.read().strip().split("\n")
                
             return model, labels
