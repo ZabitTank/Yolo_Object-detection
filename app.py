51e06c4031d4a9f516e1116b572a46f936541cb3
RabitTank <bestmatch236@gmail.com>
2022-01-05 07:22:17 +0700

    Clear cache button, setting all URL file

--- app.py
+++ app.py
@@ -2,6 +2,9 @@
 from PIL import Image
 import streamlit as st
 
+# for building button inside button
+import SessionState
+
 # for loading  and process detect
 import cv2
 import numpy as np
@@ -13,20 +16,30 @@ import urllib3
 MODEL_PATH = os.path.join(os.path.dirname(__file__), 'model')
 
 URL_PRETRAIN_MODEL = {
-"yolov4-tiny.weights" : "https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-tiny.weights",
-"yolov4-tiny.cfg": "https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4-tiny.cfg",
-"yolov4-tiny.names" : "https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names"
+"yolov4-p5.weights" : "https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-p5.weights",
+"yolov4-p5.cfg": "https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4-p5.cfg",
+"yolov4-p5.names" : "https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names"
 }
 
-PRETRAIN_MODEL_NAME = "yolov4-tiny"
+PRETRAIN_MODEL_NAME = "yolov4-p5"
 
-URL_CUSTOM_MODEL = {
-"yolov4-custom.weights" : "https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-p5.weights",
-"yolov4-custom.cfg": "https://raw.githubusercontent.com/AlexeyAB/darknet/master/cfg/yolov4-p5.cfg",
-"yolov4-custom.names" : "https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names"
+URL_CUSTOM_MODEL_1 = {
+"yolov4-tiny-custom.weights" : "https://github.com/ZabitTank/Store-training-model/raw/main/yolov4-tiny-custom_best.weights",
+"yolov4-tiny-custom.cfg": "https://raw.githubusercontent.com/ZabitTank/Store-training-model/main/yolo-obj.cfg",
+"yolov4-tiny-custom.names" : "https://raw.githubusercontent.com/ZabitTank/Store-training-model/main/obj.names"
 }
 
-MY_MODEL_NAME = "yolov4-custom"
+MY_MODEL_NAME_1 = "yolov4-tiny-custom"
+
+URL_CUSTOM_MODEL_2 = {
+"yolov4-tiny-fastfood.weights" : "https://github.com/ZabitTank/Store-training-model/raw/main/yolov4-tiny-fast-food.weights",
+"yolov4-tiny-fastfood.cfg": "https://raw.githubusercontent.com/ZabitTank/Store-training-model/main/yolov4-tiny-fast-food.cfg",
+"yolov4-tiny-fastfood.names" : "https://raw.githubusercontent.com/ZabitTank/Store-training-model/main/fast-food.names"
+}
+
+MY_MODEL_NAME_2 = "yolov4-tiny-fastfood"
+
+
 
 http = urllib3.PoolManager()
 
@@ -104,38 +117,55 @@ def show_download_model_button(model_name,url_model,files,is_update):
         if(is_update):
             st.legacy_caching.clear_cache()
         st.experimental_rerun()
-        
 
-    
+        
 def main():
     download_file = {}
     update_file = {}
     
-    st.title("Yolo detection object")
-    
     # SLIDE BAR : using to setting model
-    st.sidebar.image("./yolo_icon.png",width=100)
-    
-    st.sidebar.write("19127385(Phạm Lê Hạ)")
-    st.sidebar.write("19127114(Phạm Thành Đăng)")
-    
-    # setting model
-    st.sidebar.header("Setting Model")
-    
-    modelName = st.sidebar.selectbox("What model you want to use?",
-    ("pre-train model", "custom model")) 
-    
+    with st.sidebar:
+        st.sidebar.image("./yolo_icon.png",width=100)
+        st.sidebar.write("19127385(Phạm Lê Hạ)")
+        st.sidebar.write("19127114(Phạm Thành Đăng)")
         
-    if (modelName == "pre-train model" ):
-        handleNavigate(modelName,URL_PRETRAIN_MODEL,PRETRAIN_MODEL_NAME,download_file,update_file)
-    elif (modelName == "custom model"):
-        handleNavigate(modelName,URL_CUSTOM_MODEL,MY_MODEL_NAME,download_file,update_file)
+        # setting model
+        with st.form(key="size"):
+            st.header("Setting Model")
+            image_size = st.slider("Width and Height: ",32,896,416,32)
+            
+            modelName = st.selectbox("What model you want to use?",
+            ("pre-train model", "test model","Fast food detection"))
+            
+            loading_model_button = st.form_submit_button("Load")
+        
+        clear_cache_button = st.sidebar.button("clear cache")
+        if(clear_cache_button): st.legacy_caching.clear_cache()
+    
+    
+    ss = SessionState.get(loading_model_button = False)
+    
+    if(loading_model_button):
+        ss.loading_model_button = True
+    
+    
+    if(ss.loading_model_button):
+        st.write("current Input size: " , image_size)
+        if (modelName == "pre-train model" ):
+            st.title("Yolo pre-train yolov4-p5")
+            handleNavigate(modelName,image_size,URL_PRETRAIN_MODEL,PRETRAIN_MODEL_NAME,download_file,update_file)
+        elif (modelName == "test model"):
+            st.title("yolo-tiny custom")
+            handleNavigate(modelName,image_size,URL_CUSTOM_MODEL_1,MY_MODEL_NAME_1,download_file,update_file)
+        elif (modelName == "Fast food detection"):
+            st.title("Fast food dectection")
+            handleNavigate(modelName,image_size,URL_CUSTOM_MODEL_2,MY_MODEL_NAME_2,download_file,update_file)
 
 #####################################################################################
     
 # INPUT IMAGE PAGE: to insert image and process dectection
 
-def handleNavigate(modelName,url_model,model_original_name,download_file,update_file):
+def handleNavigate(modelName,image_size,url_model,model_original_name,download_file,update_file):
     if(check_missing_model(url_model,download_file)):
         show_download_model_button(modelName,url_model,download_file,False)
     else:
@@ -144,13 +174,13 @@ def handleNavigate(modelName,url_model,model_original_name,download_file,update_
             check_update(url_model,update_file)
         show_download_model_button(modelName,url_model,download_file,True)
             
-        model,labels = load_model(model_original_name)
+        model,labels = load_model(model_original_name,image_size)
         if (labels is None or model is None):
             st.warning("Loading failed: Path could be incorrect or model %s not existed" % modelName)
         else:
-            main_page_render(modelName,model,labels)
+            main_page_render(modelName,image_size,model,labels)
 
-def main_page_render(modelName,model,labels):
+def main_page_render(modelName,image_size,model,labels):
     #col1,col2 = st.columns(2)
     
     input_form = st.form(key="input") 
@@ -159,8 +189,8 @@ def main_page_render(modelName,model,labels):
     
     input_form.selectbox("Object can be detected on this model",labels)
     
-    confThreshold = input_form.slider("confThreshold: ",0.01,0.99,0.1,0.1)
-    nmsThreshold = input_form.slider("nmsThreshold: ", 0.001,0.99,0.4,0.1)
+    confThreshold = input_form.slider("confThreshold: ",0.0,1.0,0.1,0.1)
+    nmsThreshold = input_form.slider("nmsThreshold: ", 0.0,1.0,0.4,0.1)
     
     imgSource = selectImage(input_form)
         
@@ -168,11 +198,13 @@ def main_page_render(modelName,model,labels):
     if (detect_button):
         if imgSource is not None:
             img = load_image(imgSource)
-            st.image(detect_image(model,labels,img,confThreshold,nmsThreshold))
+            predict_img = detect_image(model,labels,img,confThreshold,nmsThreshold,image_size)
+            if(predict_img is not None):
+                st.image(predict_img)
         
 #Note: cache model
 @st.cache(allow_output_mutation=True)        
-def load_model(model_name):
+def load_model(model_name,image_size):
     with st.spinner('Loading model...'):
         try:
             cfg = model_name+".cfg"
@@ -187,7 +219,7 @@ def load_model(model_name):
             
             model = cv2.dnn_DetectionModel(net)
             
-            model.setInputSize(704, 704)
+            model.setInputSize(image_size, image_size)
             model.setInputScale(1.0 / 255)
             model.setInputSwapRB(True)
             
@@ -201,7 +233,7 @@ def load_model(model_name):
             
     
 #Note:cache image 
-@st.cache(suppress_st_warning=True)
+@st.cache(show_spinner=False)
 def load_image(image_source):
     with st.spinner('Loading image...'):
         try:
@@ -217,24 +249,29 @@ def selectImage(form):
         return image_destination
 
 
-def detect_image(model,labels,img,confThreshold,nmsThreshold):
-    img = cv2.resize(img, dsize=(704, 704), interpolation=cv2.INTER_AREA)
-    classes, confidences, boxes = model.detect(img, confThreshold, nmsThreshold)
+def detect_image(model,labels,img,confThreshold,nmsThreshold,image_size):
+    try:
+    
+        img = cv2.resize(img, dsize=(image_size, image_size), interpolation=cv2.INTER_AREA)
+        classes, confidences, boxes = model.detect(img, confThreshold, nmsThreshold)
    
-    for classId, confidence, box in zip(classes.flatten(), confidences.flatten(), boxes):
-        label = '%.2f' % confidence
-        label = '%s: %s' % (labels[classId], label)
-        labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+        for classId, confidence, box in zip(classes.flatten(), confidences.flatten(), boxes):
+            label = '%.2f' % confidence
+            label = '%s: %s' % (labels[classId], label)
+            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
         
-        left, top, width, height = box
-        top = max(top, labelSize[1])
+            left, top, width, height = box
+            top = max(top, labelSize[1])
         
-        cv2.rectangle(img, box, color=(0, 255, 0), thickness=3)
-        cv2.rectangle(img, (left, top - labelSize[1]), (left + labelSize[0], top + baseLine),
+            cv2.rectangle(img, box, color=(0, 255, 0), thickness=3)
+            cv2.rectangle(img, (left, top - labelSize[1]), (left + labelSize[0], top + baseLine),
                       (255, 255, 255), cv2.FILLED)
-        cv2.putText(img, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+            cv2.putText(img, label, (left, top), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
 
-    return img
+        return img
+    except Exception as e:
+        st.warning("can't detect this image, try change Threshold value or image size" )
+        return None
 
   
     
